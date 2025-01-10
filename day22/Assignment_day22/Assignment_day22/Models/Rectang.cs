73a48953e731778cc3e697c18b5f145ddf3abfd3
shalkRRD1 <shalini.kumari2092@gmail.com>
2025-01-10 20:35:02 +0530

    day22

---
+++ day22/Assignment_day22/Assignment_day22/Models/Rectang.cs
@@ -0,0 +1,20 @@
+﻿﻿using System;
+using System.Security.Cryptography;
+
+namespace RectangleEncapApp.Models
+{
+     class Rectang
+    {
+        public int width { get; set; }
+	public int height { get; set; }
+	public string color { get; set; }
+
+        public int CalculateArea(int w, int h, string c)
+        {
+		width = w;
+		height = h;
+		color = c;
+            return (height * width * 2);
+        }
+    }
+}
---
+++ day22/Assignment_day22/Assignment_day22/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Drawing;
+using RectangleEncapApp.Models;
+
+
+namespace RectangleEncapApp
+{
+    class RectangleTest
+    {
+        static void Main(string[] args)
+        {
+            Rectang[] rectangle1 = new Rectang[5];
+            for (int i = 0; i < 5; i++)
+            {
+                rectangle1[i] = new Rectang();
+                Console.WriteLine($"Please enter set {i + 1}");
+                rectangle1[i].width = Int32.Parse(Console.ReadLine());
+                rectangle1[i].height = Int32.Parse(Console.ReadLine());
+                rectangle1[i].color = Console.ReadLine();
+            }
+        
+            PrintInfo(rectangle1);
+
+            public void PrintInfo(Rectang[] rectan)
+            {
+                int[] area= new int[5];
+                for (int i = 0; i < 5; i++)
+                { 
+                    area[i]=rectan[i].CalculateArea(rectan[i].width, rectan[i].height, rectan[i].color);
+                    Console.WriteLine($"Area & Color of set {i+1}: {rectan[i].CalculateArea(rectan[i].width, rectan[i].height, rectan[i].color)} {rectan[i].color}");
+                }
+            }
+            
+        }
+    }
+}
