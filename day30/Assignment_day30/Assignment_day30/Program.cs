63bd5a9c3bc350ab0040d0b16dd224dc2d52d2ce
shalkRRD1 <shalini.kumari2092@gmail.com>
2025-01-27 21:41:16 +0530

    day30

--- day30/Assignment_day30/Assignment_day30/Program.cs
+++ day30/Assignment_day30/Assignment_day30/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace Assignment_day30
+﻿using System;
+
+namespace Assignment_day30
 {
-    internal class Program
+    public abstract class Product
+    {
+        public abstract int HighestProduct(int[] integer);
+    }
+    public class AdjacentProduct : ProductCalculator
+    {
+        public override int HighestProduct(int[] integer)
+        {
+            int highestProduct = int.MinValue;
+
+            for (int i = 0; i < integer.Length - 1; i++)
+            {
+                int product = integer[i] * integer[i + 1];
+                if (product > highestProduct)
+                {
+                    highestProduct = product;
+                }
+            }
+            return highestProduct;
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] integer = { 1, 3, 5, 2, 6, 2, 9 };
+            ProductCalculator calculator = new AdjacentProduct();
+            int highestProduct = calculator.HighestProduct(integer);
+            Console.WriteLine("The highest adjacent product is: " + highestProduct);
         }
     }
 }
--- day31/Assignment_day31/Assignment_day31/Program.cs
+++ day31/Assignment_day31/Assignment_day31/Program.cs
@@ -1,10 +1,53 @@
-﻿namespace Assignment_day31
+﻿using System;
+
+public interface SumInterface
 {
-    internal class Program
+    int Sum(int[] array);  
+}
+
+public abstract class SumAbstract : SumInterface
+{
+    public abstract int Sum(int[] array);  
+}
+
+public class SumClass : SumAbstract
+{
+    public override int Sum(int[] array)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+       int[] arr = new int[10];
+	   int[] sum = new int[2];
+	   
+       Console.Write("Enter the number of elements to be stored in the array: ");
+       n = Convert.ToInt32(Console.ReadLine()); 
+
+       for (int i = 0; i < arr.Length-1; i++)
+       {
+           Console.Write($"Integer[{i}]: ");  
+           arr[i] = Convert.ToInt32(Console.ReadLine());  
+       }
+
+       for (i = 0; i < arr.Length-1; i++)
+       {
+		   if(i==0 || i%2==0)
+		   {
+		   	sum[0] += arr[i]; 
+		   }
+		   else
+		   {
+		   	sum[1] += arr[i]; 
+		   }
+       }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        int[] integers = { 1, 2, 3, 4, 5 };
+        int sum = sumArray.Sum(integers);
+
+        SumInterface sumArray = new SumClass();
+		Console.Write($"Sum of alternate integers present in the array is : [{sum[0]},{sum[1]}]");
     }
 }
