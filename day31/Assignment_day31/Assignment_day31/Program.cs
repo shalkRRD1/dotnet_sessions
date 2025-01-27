c9ca79a89ff3b0c9e3e103d82ff03701b65f763d
shalkRRD1 <shalini.kumari2092@gmail.com>
2025-01-27 22:22:24 +0530

    day31_fin

--- day31/Assignment_day31/Assignment_day31/Program.cs
+++ day31/Assignment_day31/Assignment_day31/Program.cs
@@ -1,45 +1,14 @@
-<<<<<<< HEAD
 using System;
 namespace Assignment_day31
-=======
-﻿using System;
-public interface SumInterface
->>>>>>> 5aa3c217801f5638e0e6f7c910b93b9a48e71395
 {
     public interface SumInterface
     {
-<<<<<<< HEAD
         void Sum(int[] array);
     }
 
     public abstract class SumAbstract : SumInterface
     {
         public abstract void Sum(int[] array);
-=======
-       int[] arr = new int[10];
-	   int[] sum = new int[2];
-	   
-       Console.Write("Enter the number of elements to be stored in the array: ");
-       n = Convert.ToInt32(Console.ReadLine()); 
-
-       for (int i = 0; i < arr.Length-1; i++)
-       {
-           Console.Write($"Integer[{i}]: ");  
-           arr[i] = Convert.ToInt32(Console.ReadLine());  
-       }
-
-       for (i = 0; i < arr.Length-1; i++)
-       {
-		   if(i==0 || i%2==0)
-		   {
-		   	sum[0] += arr[i]; 
-		   }
-		   else
-		   {
-		   	sum[1] += arr[i]; 
-		   }
-       }
->>>>>>> 5aa3c217801f5638e0e6f7c910b93b9a48e71395
     }
 
     public class SumClass : SumAbstract
@@ -64,7 +33,6 @@ public interface SumInterface
             Console.Write($"Value array: {sumarr[0]} and {sumarr[1]}");
         }
     }
-<<<<<<< HEAD
 
     class Program
     {
@@ -80,18 +48,3 @@ public interface SumInterface
     }
 
 }
-=======
-}
-
-class Program
-{
-    static void Main()
-    {
-        int[] integers = { 1, 2, 3, 4, 5 };
-        int sum = sumArray.Sum(integers);
-
-        SumInterface sumArray = new SumClass();
-		Console.Write($"Sum of alternate integers present in the array is : [{sum[0]},{sum[1]}]");
-    }
-}
->>>>>>> 5aa3c217801f5638e0e6f7c910b93b9a48e71395
